dcc7d1c6ebc899e9378ed4b6d715e3a4f70f8943
desirekaleba <desirekaleba@gmail.com>
2020-05-18 23:03:13 +0300

    feat: Group components And Render Join Dom
    compoments grouped together in separete foloder and Join Dom
    implemented, a form that help join a specific room

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Chat.css';
+
+const Chat = () => {
+    return(
+        <h1>Chat</h1>
+    );
+};
+
+export default Chat;
