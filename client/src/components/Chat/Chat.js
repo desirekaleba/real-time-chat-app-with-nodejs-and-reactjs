3a0bcf47961c5d79b442d21f20558beb935225ec
desirekaleba <desirekaleba@gmail.com>
2020-05-22 21:22:48 +0300

    Add useEffect, to handle queryString in the chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 import './Chat.css';
 
-const Chat = () => {
+const Chat = ({ location }) => {
+    useEffect(() => {
+        const data = queryString.parse(location.search);
+        console.log('data',data,'location',location.search);
+    })
+
     return(
         <h1>Chat</h1>
     );
