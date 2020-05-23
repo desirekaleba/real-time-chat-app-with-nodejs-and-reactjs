30496b862f27cf707bff8109b3dcb068d0516203
desirekaleba <desirekaleba@gmail.com>
2020-05-23 20:30:45 +0300

    feat: Add users helper
    Handles all action about a users such as adding, removing, getting a user or getting all users who are in a particular room

---
+++ server/users.js
@@ -0,0 +1,32 @@
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    const existingUser = users.find((user) => user.room === room && user.name === name);
+
+    if (existingUser) {
+        return { error: 'Username is taken'};
+    }
+
+    const user = { id, name, room };
+
+    users.push(user);
+
+    return { user };
+
+};
+
+const removeUser = (id) => {
+
+};
+
+const getUser = () => {
+
+};
+
+const getUsersInRoom = () => {
+
+};
