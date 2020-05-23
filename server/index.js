6ecc2ec9b82e90efdcb319ca6339f31dc68ad928
desirekaleba <desirekaleba@gmail.com>
2020-05-23 20:57:15 +0300

    feat: Handle onConnection event of the socket
    The connection event is handled and can now send back messages to client when a user has joined

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -11,10 +13,22 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection');
 
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if (error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room ${user.room} `});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined`});
+
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on('SendMessage', (message, callback) => {
+
     });
 
     socket.on('disconnect', () => {
