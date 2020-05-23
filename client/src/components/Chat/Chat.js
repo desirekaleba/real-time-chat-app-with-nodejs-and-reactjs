c79114e5b43724264cf67b56ae9bb159c7098e3d
desirekaleba <desirekaleba@gmail.com>
2020-05-23 23:25:56 +0300

    feat: Add messages tag to chat
    <Messages /> tags which has everything about the messages is added to the chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,6 +3,7 @@ import queryString from 'query-string';
 import io from 'socket.io-client';
 
 import InfoBar from '../InfoBar/InfoBar';
+import Messages from '../Messages/Messages';
 import Input from '../Input/Input';
 
 import './Chat.css';
@@ -55,6 +56,7 @@ const Chat = ({ location }) => {
         <div className="outerContainer">
             <div className="container">
                 <InfoBar room={room} />
+                <Messages messages={messages} name={name} />
                 <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
         </div>
