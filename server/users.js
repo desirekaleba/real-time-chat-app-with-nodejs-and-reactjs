dbb79fa9b36d1e508f14141548759fc2a6173033
desirekaleba <desirekaleba@gmail.com>
2020-05-23 20:35:09 +0300

    feat: Add removeUser
    a helper to remove a specific user

--- server/users.js
+++ server/users.js
@@ -19,7 +19,11 @@ const addUser = ({ id, name, room }) => {
 };
 
 const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
 
+    if (index !== -1) {
+        return users.splice(index, 1)[0];
+    }
 };
 
 const getUser = () => {
