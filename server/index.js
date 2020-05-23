ac65ba1dd88c03ca90df89999e256a2b22c0d48a
desirekaleba <desirekaleba@gmail.com>
2020-05-23 23:24:01 +0300

    feat: Add roomData event emmiter
    When we have new data in a room, the roomData event emmiter is fired to notify the room about the changes

--- server/index.js
+++ server/index.js
@@ -24,19 +24,26 @@ io.on('connection', (socket) => {
 
         socket.join(user.room);
 
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+
         callback();
     });
 
-    socket.on('SendMessage', (message, callback) => {
+    socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id);
 
         io.to(user.room).emit('message', { user: user.name, text: message});
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
 
         callback();
     });
 
     socket.on('disconnect', () => {
-        console.log('User has left!!!');
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.`});
+        }
     });
 });
 
