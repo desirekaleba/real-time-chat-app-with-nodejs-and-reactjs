8480476deac9d09bd8d23f70530fece77a621c38
desirekaleba <desirekaleba@gmail.com>
2020-05-24 13:00:05 +0300

    feat: Add roomData event emitter to know when a new user has joined

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
@@ -12,17 +13,21 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
+app.use(cors());
+app.use(router);
+
+io.on('connect', (socket) => {
 
     socket.on('join', ({ name, room }, callback) => {
         const { error, user } = addUser({ id: socket.id, name, room });
 
         if (error) return callback(error);
 
+        socket.join(user.room);
+
         socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room ${user.room} `});
         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined`});
 
-        socket.join(user.room);
 
         io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
 
@@ -33,7 +38,7 @@ io.on('connection', (socket) => {
         const user = getUser(socket.id);
 
         io.to(user.room).emit('message', { user: user.name, text: message});
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        //io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
 
         callback();
     });
@@ -43,10 +48,11 @@ io.on('connection', (socket) => {
 
         if (user) {
             io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.`});
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
         }
     });
 });
 
-app.use(router);
+
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
