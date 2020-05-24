a6de3ca412e9f78c7f26ff0cca16cc5c0492a1ae
desirekaleba <desirekaleba@gmail.com>
2020-05-24 18:25:10 +0300

    Add project doployment URLs to info file

---
+++ client/build/precache-manifest.2cf2b9d71c6bc90f14c7979d1ab8d573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb66433ec3a81ed5c1fe43119ab1238c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8bd5e3c5c28779e678b",
+    "url": "/static/css/main.d06af0d4.chunk.css"
+  },
+  {
+    "revision": "cf0ba358c9ff81a6a873",
+    "url": "/static/js/2.7419e5e5.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.7419e5e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8bd5e3c5c28779e678b",
+    "url": "/static/js/main.7bb2fd2f.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
